2e35c7e2ff542ce23ba820a32bc67deadd391402
Imoh Bassey <Basseyimoh3012@gmail.com>
2025-03-08 23:06:22 +0100

    fixes

--- trustvote-frontend/vite.config.js
+++ trustvote-frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import polyfillNode from 'vite-plugin-polyfill-node';
+import { polyfillNode } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
