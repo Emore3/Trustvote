36f542292743a816d90b682e329c48d81a9d09a3
Imoh Bassey <Basseyimoh3012@gmail.com>
2025-03-23 00:43:17 +0100

    reduced the amount of eth sent to wallet

--- trustvote-backend/services/ethersService.js
+++ trustvote-backend/services/ethersService.js
@@ -15,7 +15,7 @@ const votingSystemAbi = [
 const votingSystemContract = new ethers.Contract(votingSystemAddress, votingSystemAbi, fundingWallet);
 
 // Define the fund amount (0.05 ether)
-const FUND_AMOUNT = ethers.parseEther("0.05");
+const FUND_AMOUNT = ethers.parseEther("0.03");
 
 /**
  * Sends funds to the specified wallet address.
