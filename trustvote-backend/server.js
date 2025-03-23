a75ddc521dacd1ffa2e11ea6a6e1b09df428a14c
Imoh Bassey <Basseyimoh3012@gmail.com>
2025-03-23 10:33:14 +0100

    set up cors policy for backend

--- trustvote-backend/server.js
+++ trustvote-backend/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 // Import routes
 const authRoutes = require("./routes/auth");
+
+app.use(cors());
 
 const app = express();
 app.use(express.json());
