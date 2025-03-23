07fdeadebba6d4cd144c0245828352ac52a13a01
Imoh Bassey <Basseyimoh3012@gmail.com>
2025-03-23 01:02:40 +0100

    fix: footer import issue

--- trustvote-frontend/src/App.jsx
+++ trustvote-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 import Home from "./components/Home"
 import AdminDashboard from "./components/AdminDashboard"
 import VoterDashboard from "./components/VoterDashboard"
