38f69321f49fc30ccb547cad6e231565f8f7bed4
Imoh Bassey <Basseyimoh3012@gmail.com>
2025-03-23 10:36:55 +0100

    removed unneeded mongoose params

--- trustvote-backend/server.js
+++ trustvote-backend/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, {})
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
